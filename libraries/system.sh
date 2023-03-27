661b3833e926817df8b36ed2fc3464662b37623f
ikygoose <isaac.p.weigel@gmail.com>
2023-03-27 12:35:23 -0500

    #38 changing how the key map is set on new system

--- libraries/system.sh
+++ libraries/system.sh
@@ -411,13 +411,19 @@ function system::set_timezone()
 #
 # Source:
 #   N/A
+#
+#   TODO: figure out how to set x11 /mnt/etc/X11/xorg.conf.d/00-keyboard.conf correctly (if x11 is being installed)
+#   https://pastebin.com/7e0JmhHR
 ###################################################################################################
 function system::set_key_map()
 {
     local root_mount="$1"
     local key_map="$2"
 
-    arch-chroot "${root_mount}" "localectl" "set-keymap" "${key_map}"
+    echo "KEYMAP=${key_map}" > "${root_mount}/etc/vconsole.conf"
+
+    # use this for x11 keyboard config?
+    #systemd-nspawn -b -D "${root_mount}" "localectl" "--no-convert" "set-keymap" "${key_map}"
 }
 
 ###################################################################################################
